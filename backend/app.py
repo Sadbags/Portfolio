0240a7bfac80a96de6109b45f00755b34e0ba503
Alex Guadalupe <9035@holbertonstudents.com>
2024-09-06 13:52:22 -0400

    updating info

--- backend/app.py
+++ backend/app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect, url_for, jsonify
 
 # Initialize the Flask application
 app = Flask(__name__)
@@ -6,7 +6,13 @@ app = Flask(__name__)
 # Define a route for the home page
 @app.route('/')
 def home():
-    return 'Welcome to quickr'
+    return render_template('home.html')
+
+# Define a route for the login page
+@app.route('/login')
+def login():
+    return render_template('login.html')
+
 # Runs the application
 if __name__ == '__main__':
     app.run(debug=True)
